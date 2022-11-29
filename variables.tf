b339300ab6b027613f9790a3986cba03ab3db8e1
JoseMotto-sg <jose.motto@stratusgrid.com>
2022-11-29 11:16:56 +0100

    add feature required_linear_history

--- variables.tf
+++ variables.tf
@@ -23,3 +23,9 @@ variable "create" {
   type        = bool
   default     = true
 }
+
+variable "required_linear_history" {
+  description = "If it needs to use the linear history feature"
+  type        = bool
+  default     = true
+}
