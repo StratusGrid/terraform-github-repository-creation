ff7e463ce9afcf477c04a982c4c36db17f020f99
DJKaswa <74205036+DJKaswa@users.noreply.github.com>
2022-12-13 11:08:39 -0600

    Updates README file.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
+# Repositories map object definitions with their defaults to use if none of the optional values are explicitely entered
 variable "repositories" {
   description = "A map of Repository objects and their source template repos"
   type = map(object({
-    repository_template             = optional(string)
-    required_approving_review_count = optional(number)
-    visibility                      = optional(string)
+    repository_template             = optional(string, "")
+    required_approving_review_count = optional(number, 2)
+    visibility                      = optional(string, "private")
   }))
 }
 
